05a327012684d169048d0219bf70c2ea25bfe1c2
psdfeng <psdfeng@live.com>
2017-12-22 01:05:07 -0800

    1

--- 2048/a.py
+++ 2048/a.py
@@ -1,2 +1,15 @@
-import game
+#-*- coding:utf-8 -*-
 
+import curses
+from random import randrange,choice
+from collections import defaultdict
+
+letter_codes=[ord(ch) for ch in 'WASDRQwasdrq']
+actions=['Up','Left','Down','Right','Restart','Exit']
+actions_dict=dict(zip(letter_codes,actions*2))
+
+def get_user_action(keyboard):
+    char="N"
+    while char not in actions_dict:
+        char=keyboard.getch()
+    return actions_dict[char]
