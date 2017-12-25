250034316d55e5857b72e880ee370ad9475a3a7c
psdfeng <psdfeng@live.com>
2017-12-24 18:41:21 -0800

    .

--- 2048/a.py
+++ 2048/a.py
@@ -4,8 +4,11 @@ import curses
 from random import randrange,choice
 from collections import defaultdict
 
+#user input
 letter_codes=[ord(ch) for ch in 'WASDRQwasdrq']
+#user actions
 actions=['Up','Left','Down','Right','Restart','Exit']
+#input actions 
 actions_dict=dict(zip(letter_codes,actions*2))
 
 def get_user_action(keyboard):
@@ -13,3 +16,9 @@ def get_user_action(keyboard):
     while char not in actions_dict:
         char=keyboard.getch()
     return actions_dict[char]
+def transpose(field):
+
+
+def main(stdscr):
+    def init():
+        #
